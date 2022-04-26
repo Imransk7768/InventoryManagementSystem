84dcb401b87685a4c1ae196023aa7d55c1979c91
Imransk7768 <imransk7768@gmail.com>
2022-04-27 01:01:52 +0530

    [Imran] Added: Calculate TotalPrice Of Items

---
+++ OOPS/InventoryData_Grains/InventoryManagement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using OOPS.InventoryData_Grains;
+
+namespace OOPS
+
+{
+    public class InventoryManagement
+    {
+        public void DisplayInventory(string filePath)
+        {
+            StreamReader reader = new StreamReader(filePath);
+            {
+                var json = reader.ReadToEnd();
+                var inventory = JsonConvert.DeserializeObject<List<InventoryData>>(json);
+                Console.WriteLine("Name" + "\t" + "Weight" + "\t" + "PricePerKg" + "\t" + "TotalPrice");
+                foreach (var data in inventory)
+                {
+                    Console.WriteLine(data.ItemName + "\t" + data.ItemWeight + "\t" + data.ItemPricePerKg + "\t\t" + (data.ItemWeight * data.ItemPricePerKg));
+                }
+            }
+        }
+    }
+}
--- OOPS/Program.cs
+++ OOPS/Program.cs
@@ -2,11 +2,29 @@
 
 namespace OOPS
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine(">>>>>  Welcome to INVENTORY MANAGEMENT System  <<<<<");
+            const string INVENTORY_DATA_PATH = @"H:\Assignments\InventoryManagementSystem\OOPS\InventoryData_Grains\Inventory.json";
+            InventoryManagement InData = new InventoryManagement();
+            Random random = new Random();
+            bool check = true;
+            while (check)
+            {
+                Console.WriteLine("1.Display Data");
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        InData.DisplayInventory(INVENTORY_DATA_PATH);
+                        break;
+                    default:
+                        Console.WriteLine("Program Ends");
+                        check = false;
+                        break;
+                }
+            }
         }
     }
 }
