7d61cae30615f02e5d9407dff7e13392b7cce000
Imransk7768 <imransk7768@gmail.com>
2022-04-29 02:22:15 +0530

    [Imran] Refactor: Refactor Inventory Data Management_Grains

--- OOPS/Program.cs
+++ OOPS/Program.cs
@@ -8,23 +8,20 @@ namespace OOPS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(">>>>>  Welcome to INVENTORY MANAGEMENT System  <<<<<");
-            const string INVENTORY_DATA_PATH = @"H:\Assignments\InventoryManagementSystem\OOPS\InventoryData_Grains\Inventory.json";
-            //const string INVENTORY_DATA_PATH = @"H:\Assignments\InventoryManagementSystem\OOPS\InventoryDataManagenemt_Grains\InventoryManage.json";
+            Console.WriteLine(">>>>>  Welcome to INVENTORY DATA MANAGEMENT System  <<<<<");
+            const string INVENTORY_DATA_PATH = @"H:\Assignments\InventoryManagementSystem\OOPS\InventoryDataManagenemt_Grains\InventoryManage.json";
             InventoryManagement InData = new InventoryManagement();
             InventoryManager manager = new InventoryManager();
             InventoryData InvData = new InventoryData();
-            Console.WriteLine("1. Display Data\n2. Add Data\n3. Edit Data \n-in Inventory");
+            Console.WriteLine("1. Add InventoryData\n2. Edit InventoryData\n ");
             bool check = true;
             while (check)
             {
                 switch (Convert.ToInt32(Console.ReadLine()))
                 {
+
                     case 1:
-                        InData.DisplayInventory(INVENTORY_DATA_PATH);
-                        break;
-                    case 2:
-                       Console.WriteLine("Add InventoryData & InventoryList");
+                        Console.WriteLine("Add InventoryData & InventoryList");
                         string inventory_Name = Console.ReadLine();
                         Console.WriteLine("Enter Inventory Details : ");
                         InventoryData data = new InventoryData();
@@ -33,13 +30,14 @@ namespace OOPS
                         data.ItemPricePerKg = Convert.ToInt32(Console.ReadLine());
                         manager.AddInventory(data, inventory_Name, INVENTORY_DATA_PATH);
                         break;
-                    case 3:
+                    case 2:
                         Console.WriteLine("Enter the Inventory List Name & GrainsName : ");
                         string inventoryName = Console.ReadLine();
                         string item = Console.ReadLine();
                         manager.EditInventory(inventoryName, item, INVENTORY_DATA_PATH);
                         InData.DisplayInventory(INVENTORY_DATA_PATH);
                         break;
+
                     default:
                         Console.WriteLine("Program Ends");
                         check = false;
